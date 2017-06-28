37bc542f5864e46b212d1db6bf6913e740cfb54f
serjt <tanaki.93@gmail.com>
2017-06-28 23:15:14 +0600

    new commit

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,49 @@ from django.contrib import admin
 # Register your models here.
 from main.models import *
 
-admin.site.register(Kabar)
-admin.site.register(Npa)
-admin.site.register(Education)
-admin.site.register(Atestation)
-admin.site.register(Protocol)
+
+class KabarAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Kabar
+
+    list_display = 'title icon date'.split()
+
+    def icon(self, obj):
+        return '<img src="%s" style = "width:50px; height=50px;" />' % obj.image.url
+
+    icon.allow_tags = True
+
+
+class NpaAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Npa
+
+    list_display = 'name number date'.split()
+
+
+class EducationAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Education
+
+    list_display = 'name category date'.split()
+
+
+class AttestationAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Atestation
+
+    list_display = 'name number date'.split()
+
+
+class ProtocolAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Protocol
+
+    list_display = 'name date'.split()
+
+
+admin.site.register(Kabar, KabarAdmin)
+admin.site.register(Npa, NpaAdmin)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Atestation, AttestationAdmin)
+admin.site.register(Protocol, ProtocolAdmin)
--- main/models.py
+++ main/models.py
@@ -14,77 +14,94 @@ def image_upload_to(instance, filename):
 class Kabar(models.Model):
     class Meta:
         ordering = '-date'.split()
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to=image_upload_to)
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    image = models.ImageField(upload_to=image_upload_to, verbose_name='Картинка')
     text = RedactorField(verbose_name='Текст',
                          upload_to=image_upload_to,
                          # redactor_options={'buttons': ['image'],},
                          allow_image_upload=True,
                          allow_file_upload=True)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата')
 
     def __unicode__(self):
         return self.title
 
 
 class Npa(models.Model):
-    name = models.CharField(max_length=100)
-    number = models.CharField(max_length=100)
+    class Meta:
+        ordering = '-date'.split()
+        verbose_name = 'НПА'
+        verbose_name_plural = 'НПА'
+    name = models.CharField(max_length=100, verbose_name='Приказ')
+    number = models.CharField(max_length=100, verbose_name='Номер')
     text = RedactorField(verbose_name='Текст',
                          upload_to=image_upload_to,
                          # redactor_options={'buttons': ['image'],},
                          allow_image_upload=True,
                          allow_file_upload=True)
-    date = models.DateTimeField(auto_now=True)
-    file = models.FileField(upload_to=image_upload_to, blank=True, null=True)
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата')
+    file = models.FileField(upload_to=image_upload_to, blank=True, null=True, verbose_name='Файл')
 
     def __unicode__(self):
         return self.name
 
 
 class Education(models.Model):
+    class Meta:
+        ordering = '-date'.split()
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
     choices = (
         ('ПДМО', 'ПДМО'),
         ('НМО', 'НМО')
     )
     category = models.CharField(choices=choices,
-                                max_length=100)
-    name = models.CharField(max_length=100)
+                                max_length=100, verbose_name='Категория')
+    name = models.CharField(max_length=100, verbose_name='Заголовок')
     text = RedactorField(verbose_name='Текст',
                          upload_to=image_upload_to,
                          # redactor_options={'buttons': ['image'],},
                          allow_image_upload=True,
                          allow_file_upload=True)
-    date = models.DateTimeField(auto_now=True)
-    file = models.FileField(upload_to=image_upload_to, blank=True, null=True)
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата')
+    file = models.FileField(upload_to=image_upload_to, blank=True, null=True, verbose_name='Файл')
 
     def __unicode__(self):
         return self.name
 
 
 class Atestation(models.Model):
-    name = models.CharField(max_length=100)
+    class Meta:
+        ordering = '-date'.split()
+        verbose_name = 'аттестация'
+        verbose_name_plural = 'аттестации'
+    name = models.CharField(max_length=100, verbose_name='Приказ')
+    number = models.CharField(max_length=100, verbose_name='Номер')
     text = RedactorField(verbose_name='Текст',
                          upload_to=image_upload_to,
                          # redactor_options={'buttons': ['image'],},
                          allow_image_upload=True,
                          allow_file_upload=True)
-    date = models.DateTimeField(auto_now=True)
-    number = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата')
 
     def __unicode__(self):
         return self.name
 
 
 class Protocol(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    class Meta:
+        ordering = '-date'.split()
+        verbose_name = 'протокол'
+        verbose_name_plural = 'протоколы'
+    name = models.CharField(max_length=100, verbose_name='Отделение')
     text = RedactorField(verbose_name='Текст',
                          upload_to=image_upload_to,
                          # redactor_options={'buttons': ['image'],},
                          allow_image_upload=True,
                          allow_file_upload=True)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата')
 
     def __unicode__(self):
         return self.name
