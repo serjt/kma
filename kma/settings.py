1ef838116a74399f9b58b9884456cc47fae5adde
serjt <tanaki.93@gmail.com>
2017-06-29 20:58:19 +0600

    new commit aa

--- kma/settings.py
+++ kma/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '6n)^so5jeksyoc!0#jut--d88e7&+*1mm&4ddq+z63&&p6=trk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
